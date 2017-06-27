d55ad755552e0c5d883744555ccd0a8be60c777c
Daniel Kucera <daniel.kucera@gmail.com>
2017-06-28 00:24:43 +0200

    create words of all lengths

--- anagram.py
+++ anagram.py
@@ -6,14 +6,14 @@ input_set = sys.argv[1:]
 
 def get_combinations(prefix, char_set):
 #    print "gc", prefix, char_set
-    if len(char_set) == 1:
-        print prefix + char_set[0]
     for char in char_set:
         subset = char_set[:]
         subset.remove(char)
         subprefix = prefix + char
+        print subprefix
 #        print char_set, subprefix, subset
-        get_combinations(subprefix, subset)
+        if len(subset) > 0:
+            get_combinations(subprefix, subset)
 
 print input_set
 print get_combinations("",input_set)
