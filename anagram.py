9f23b99b74287c615c8ba36b7a3fea26438d35ac
Daniel Kucera <daniel.kucera@gmail.com>
2017-06-28 08:17:23 +0200

    compare word with db of words

--- anagram.py
+++ anagram.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python
 
 import sys
+import sqlite3
 
 input_set = sys.argv[1:]
+
+conn = sqlite3.connect("wordlist.db")
+c = conn.cursor()
+
+def check_word(word):
+#    print word
+    c.execute("SELECT * FROM words WHERE word = ?", (word,))
+    if c.fetchone() != None:
+        print word
 
 def get_combinations(prefix, char_set):
 #    print "gc", prefix, char_set
@@ -10,7 +20,7 @@ def get_combinations(prefix, char_set):
         subset = char_set[:]
         subset.remove(char)
         subprefix = prefix + char
-        print subprefix
+        check_word(subprefix)
 #        print char_set, subprefix, subset
         if len(subset) > 0:
             get_combinations(subprefix, subset)
