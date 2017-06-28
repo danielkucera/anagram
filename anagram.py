a8af8c41a2dcad493905e2303b2a19f31b4fc52a
Daniel Kucera <kucera@apptocloud.com>
2017-06-28 09:55:21 +0200

    count dict and combinations

--- anagram.py
+++ anagram.py
@@ -8,13 +8,25 @@ input_set = sys.argv[1:]
 conn = sqlite3.connect("wordlist.db")
 c = conn.cursor()
 
+def get_comb_count(n):
+    count = 0
+    last = 1
+    for i in range(0, n):
+        last *= n-i
+        count += last
+    return count
+
+def get_dict_len():
+    c.execute("SELECT COUNT(*) AS count FROM words")
+    return c.fetchone()[0]
+
 def check_word(word):
 #    print word
     c.execute("SELECT * FROM words WHERE word = ?", (word,))
     if c.fetchone() != None:
         print word
 
-def get_combinations(prefix, char_set):
+def check_combinations(prefix, char_set):
 #    print "gc", prefix, char_set
     for char in char_set:
         subset = char_set[:]
@@ -23,9 +35,12 @@ def get_combinations(prefix, char_set):
         check_word(subprefix)
 #        print char_set, subprefix, subset
         if len(subset) > 0:
-            get_combinations(subprefix, subset)
+            check_combinations(subprefix, subset)
 
-print input_set
-print get_combinations("",input_set)
+comb_count = get_comb_count(len(input_set))
+dict_len = get_dict_len()
+print comb_count, dict_len
 
+if comb_count < dict_len:
+    check_combinations("",input_set)
 
