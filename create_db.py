36c8eb9a4e6402d053bd61fc0f4aa90b280467e4
Daniel Kucera <kucera@apptocloud.com>
2017-06-30 15:32:32 +0200

    load file from parameter, use unicode

--- create_db.py
+++ create_db.py
@@ -1,17 +1,21 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import sqlite3
+import sys
 
-f = open("words.txt", "r")
+f = open(sys.argv[1], "r", encoding="utf-8")
 
 conn = sqlite3.connect("wordlist.db")
 c = conn.cursor()
 
-c.execute("CREATE TABLE words (word text)")
-c.execute("CREATE INDEX words_index ON words(word)")
+try:
+    c.execute("CREATE TABLE words (word text)")
+    c.execute("CREATE INDEX words_index ON words(word)")
+except:
+    pass
 
 for line in f:
-    print "line",line[:-1]
+    print("line",line[:-1])
     c.execute("INSERT INTO words VALUES(?)", (line.rstrip(),))
 
 conn.commit()
